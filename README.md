6d834c5807a6aa35b673613530f96b083f485c08
lusmattg <lusmattg@users.noreply.github.com>
2023-03-01 22:34:06 -0500

    add 3 different buttons, and styling

--- README.md
+++ README.md
@@ -1,11 +1,27 @@
 # chromeext
 
+CHATGPT Summarize Extension
 
-EXTENSION IDEAS
+AUTHORS: Mattholee & lusmattg
 
+INSTRUCTIONS:
+    * Update content.js to enter your chatgpt API key as OPENAI_API_KEY
+    * Go to chrome://extensions/, enable Developer Mode, and Load Unpacked this folder
+    * Visit any site and look in the bottom right
 
+PURPOSE: 
 
-
-FEATURE LIST
-
-
+CHROME EXTENSION TODO LIST
+    [x] ask prompt & get answer
+    [x] display the answer in a pretty way
+    [x] button to send prompt
+    [x] highlight text and use THAT as the prompt
+        [x] if no text is highlighted, what happens?
+    [ ] different kinds of prompts (learn more, summarize, etc)
+    [x] style
+    [ ] display as a dom element the history of your questions
+    [ ] icon
+    [ ] name
+    [?] what model to use? davinci vs turbo?
+    [/] spinning icon while it's thinking
+ 
--- content.js
+++ content.js
@@ -1,36 +1,119 @@
-console.log('hello world');
-
 //create a div that will contain our button and answer
+const chatContainer = document.createElement('div');
+chatContainer.setAttribute('id', 'chatContainer')
+chatContainer.setAttribute('style', 'width: 85%; height: 30%; position: fixed; bottom:16px; right: 16px;')
+document.body.appendChild(chatContainer);
+
 const chatInterface = document.createElement('div');
 chatInterface.setAttribute('id', 'chatInterface')
-chatInterface.setAttribute('style', 'width: 150px; height: 150px; position: absolute; bottom:16px; right: 16px; border: 1px solid black')
-document.body.appendChild(chatInterface);
+chatInterface.setAttribute('style', 'width: 80%; height: 30%; position: fixed; bottom:16px; right: 16px; background-Color: white; border: 1px solid black; border-radius: 10px 10px 10px 10px; overflow: scroll; font-size: 32px;')
+chatContainer.appendChild(chatInterface);
+
+//create a h1 that keeps track of the question being asked
+const chatQuestion = document.createElement('h2');
+chatQuestion.setAttribute('id', 'chatQuestion')
+chatQuestion.setAttribute('style','padding:4px')
+document.querySelector('#chatInterface').appendChild(chatQuestion);
 
 //create messages that lives on page and when clicked runs ChatGPT 
-const chatHistory = document.createElement('p');
+const chatHistory = document.createElement('h4');
 chatHistory.setAttribute('id', 'chatHistory')
-chatHistory.setAttribute('style', 'width: 100px; height: 100px; border: 1px solid black')
+chatHistory.setAttribute('style', 'width: 80%; height: 100%; font-size: 20px; padding: 8px');
 document.querySelector('#chatInterface').appendChild(chatHistory);
+
+const toggleButton = document.createElement('button')
+toggleButton.innerText = 'Toggle';
+toggleButton.setAttribute('id', 'toggleButton')
+toggleButton.setAttribute('style', 'width: 75px; height: 25px; position:absolute; top:0; right:25px;')
+toggleButton.addEventListener('click', function() {
+    let div = document.getElementById('chatInterface')
+    if (div.style.display === 'none') {
+        div.style.display = 'block'
+    } 
+    else {div.style.display = 'none'}
+})
+chatContainer.appendChild(toggleButton);
 
 //create button that lives on page and when clicked runs ChatGPT 
 const chatGPTButton = document.createElement('button');
-chatGPTButton.innerText = 'ChatGPT';
+chatGPTButton.innerText = 'Explain';
 chatGPTButton.setAttribute('id', 'chatGPTButton')
-chatGPTButton.setAttribute('style', 'width: 50px; height: 50px; position: absolute; bottom:16px; right: 16px')
+chatGPTButton.setAttribute('style', 'width: 100px; height: 50px; position: absolute; bottom:25px; right: 25px')
 chatGPTButton.addEventListener('click',() => {
-    console.log('button clicked');
     const selectedText = window.getSelection().toString();
-    console.log('checking ' + selectedText)
-    queryChatGPT(selectedText);
+    // document.getElementById('chatQuestion').innerText = ('Summarize ' + selectedText); 
+    // console.log('checking ' + selectedText)
+    // document.querySelector('#chatHistory').innerHTML = 'Thinking...'
+    document.getElementById('chatInterface').style.display = "block";
+    if (selectedText.length === 0) {
+        document.getElementById('chatQuestion').innerText = ('How to highlight text on your browser:'); 
+        document.querySelector('#chatHistory').innerHTML = 'Thinking...'
+        queryChatGPT('How to highlight a text on your browser');
+    }
+    else {
+        document.getElementById('chatQuestion').innerText = ('Explain ' + selectedText); 
+        document.querySelector('#chatHistory').innerHTML = 'Thinking...'
+        queryChatGPT('Explain ' + selectedText);
+    }
+    
 })
-document.querySelector('#chatInterface').appendChild(chatGPTButton);
+document.querySelector('#chatContainer').appendChild(chatGPTButton);
 
+//Second button that gives you examples 
+const examplesButton = document.createElement('button');
+examplesButton.innerText = 'Examples';
+examplesButton.setAttribute('id', 'examplesButton')
+examplesButton.setAttribute('style', 'width: 100px; height: 50px; position: absolute; bottom:75px; right: 25px')
+examplesButton.addEventListener('click',() => {
+    const selectedText = window.getSelection().toString();
+    // document.getElementById('chatQuestion').innerText = ('Summarize ' + selectedText); 
+    // console.log('checking ' + selectedText)
+    // document.querySelector('#chatHistory').innerHTML = 'Thinking...'
+    document.getElementById('chatInterface').style.display = "block";
+    if (selectedText.length === 0) {
+        document.getElementById('chatQuestion').innerText = ('How to highlight text on your browser:'); 
+        document.querySelector('#chatHistory').innerHTML = 'Thinking...'
+        queryChatGPT('How to highlight a text on your browser');
+    }
+    else {
+        document.getElementById('chatQuestion').innerText = ('Here are examples of ' + selectedText); 
+        document.querySelector('#chatHistory').innerHTML = 'Thinking...'
+        queryChatGPT('Give me examples of ' + selectedText);
+    }
+    
+})
+document.querySelector('#chatContainer').appendChild(examplesButton);
+
+//Third button that explains concepts like a pirate
+const piratefy = document.createElement('button');
+piratefy.innerText = 'Yer a Pirate';
+piratefy.setAttribute('id', 'piratefy')
+piratefy.setAttribute('style', 'width: 100px; height: 50px; position: absolute; bottom:125px; right: 25px')
+piratefy.addEventListener('click',() => {
+    const selectedText = window.getSelection().toString();
+    // document.getElementById('chatQuestion').innerText = ('Summarize ' + selectedText); 
+    // console.log('checking ' + selectedText)
+    // document.querySelector('#chatHistory').innerHTML = 'Thinking...'
+    document.getElementById('chatInterface').style.display = "block";
+    if (selectedText.length === 0) {
+        document.getElementById('chatQuestion').innerText = ('Arrr, ye lookin\' to be highlightin\' some text on yer browser?'); 
+        document.querySelector('#chatHistory').innerHTML = 'Ye\'ll need to select the text ye want highlighted and then press CTRL+C to copy it and then press CTRL+V to paste it. That\'ll do the trick!'
+        //queryChatGPT('How to highlight a text on your browser');
+    }
+    else {
+        document.getElementById('chatQuestion').innerText = ('How a pirate would explain ' + selectedText); 
+        document.querySelector('#chatHistory').innerHTML = 'Thinking...'
+        queryChatGPT('Explain \"' + selectedText + '\" as if you were a pirate');
+    }
+    
+})
+document.querySelector('#chatContainer').appendChild(piratefy);
 
 
 
     //implementation: have a event listener that listens to onClick of button, invoking everything below: 
 
-function queryChatGPT(highlight) {
+function queryChatGPT(question) {
     //ChatGPT Core functionality  
     const OPENAI_API_KEY = ''
 
@@ -41,7 +124,7 @@ function queryChatGPT(highlight) {
     // "prompt": "how do I run node.js in a chrome extension?",
     const body = {
         "model": "text-davinci-003",
-        "prompt": "summarize " + highlight,
+        "prompt": question,
         "temperature": 0.7,
         "max_tokens": 256,
         "top_p": 1,
@@ -49,8 +132,6 @@ function queryChatGPT(highlight) {
         "presence_penalty": 0
     }
 
-    console.log('Attempting to fetch from URL');
-    console.log('Asking question: ' + body.prompt)
     fetch(url,{
         //todo: what are we passing in
         method: 'POST',
@@ -61,7 +142,6 @@ function queryChatGPT(highlight) {
     .then((response) => response.json() )
     .then((data) => {
         //logic goes here after a success
-        console.log("Success:", data.choices[0]['text']);
         document.querySelector('#chatHistory').innerHTML = data.choices[0]['text'];
     })
     .catch((error) => {
